ce91c815a65d089783a348a8dea3c8520066081e
nimaster <nimaster@cisco.com>
2016-10-20 13:53:43 +0100

    1st commit

---
+++ README.md
@@ -0,0 +1,2 @@
+#Ping Sweep on Cisco Nexus 7K
+This is a simple script to ping all host addresses in a /24 subnet on a Cisco Nexus 7k
---
+++ ping-sweep24.py
@@ -0,0 +1,8 @@
+y = 1
+n = ('25.255.0.')
+while y<254:
+    z = y
+    x = w+str(y)
+    cli('ping %s count 2 vrf management timeout 1' %x)
+    y = z +1
+    print ('\n')
